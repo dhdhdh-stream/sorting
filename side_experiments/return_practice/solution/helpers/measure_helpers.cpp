468c4e012c49180c19f71303673e2bed475b5a6b
dhdhdh <dhdhdhstream@gmail.com>
2026-05-29 16:12:49 -0400

    Day #1404: Return practice. Adding more verification.

--- side_experiments/return_practice/solution/helpers/measure_helpers.cpp
+++ side_experiments/return_practice/solution/helpers/measure_helpers.cpp
@@ -1,9 +1,12 @@
 #include "solution_helpers.h"
 
+#include <iostream>
+
 #include "network.h"
 #include "run.h"
 #include "test_indirect.h"
 #include "world_model.h"
+#include "world_model_helpers.h"
 #include "wrapper.h"
 
 using namespace std;
@@ -63,4 +66,220 @@ void measure_helper(Wrapper* wrapper,
 	misguess_average = sum_misguess / MEASURE_NUM_ITERS;;
 
 	delete problem_type;
+}
+
+vector<vector<double>> TEST_OBS_R1{
+	vector<double>{0.0},
+	vector<double>{0.0}
+};
+vector<int> TEST_ACTIONS_R1{1};
+
+vector<vector<double>> TEST_OBS_R2{
+	vector<double>{1.0},
+	vector<double>{0.0}
+};
+vector<int> TEST_ACTIONS_R2{1};
+
+vector<vector<double>> TEST_OBS_R3{
+	vector<double>{0.0},
+};
+vector<int> TEST_ACTIONS_R3{};
+
+vector<vector<double>> TEST_OBS_R4{
+	vector<double>{0.0},
+	vector<double>{0.0},
+	vector<double>{0.0}
+};
+vector<int> TEST_ACTIONS_R4{1, 1};
+
+vector<vector<double>> TEST_OBS_R5{
+	vector<double>{0.0},
+	vector<double>{0.0},
+	vector<double>{0.0},
+	vector<double>{0.0}
+};
+vector<int> TEST_ACTIONS_R5{1, 1, 0};
+
+vector<vector<double>> TEST_OBS_R6{
+	vector<double>{1.0},
+	vector<double>{0.0},
+	vector<double>{0.0},
+	vector<double>{0.0}
+};
+vector<int> TEST_ACTIONS_R6{0, 1, 0};
+
+void measure_test(Wrapper* wrapper) {
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R1.size(); a_index++) {
+			obs_helper(TEST_OBS_R1[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R1[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R1.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R1 predicted: " << sum_score << endl;
+	}
+
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R2.size(); a_index++) {
+			obs_helper(TEST_OBS_R2[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R2[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R2.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R2 predicted: " << sum_score << endl;
+	}
+
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R3.size(); a_index++) {
+			obs_helper(TEST_OBS_R3[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R3[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R3.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R3 predicted: " << sum_score << endl;
+	}
+
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R4.size(); a_index++) {
+			obs_helper(TEST_OBS_R4[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R4[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R4.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R4 predicted: " << sum_score << endl;
+	}
+
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R5.size(); a_index++) {
+			obs_helper(TEST_OBS_R5[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R5[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R5.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R5 predicted: " << sum_score << endl;
+	}
+
+	{
+		vector<double> state(wrapper->world_model->num_states, 0.0);
+		for (int a_index = 0; a_index < (int)TEST_ACTIONS_R6.size(); a_index++) {
+			obs_helper(TEST_OBS_R6[a_index],
+					   state,
+					   wrapper);
+
+			action_helper(TEST_ACTIONS_R6[a_index],
+						  state,
+						  wrapper);
+		}
+
+		obs_helper(TEST_OBS_R6.back(),
+				   state,
+				   wrapper);
+
+		double sum_score = 0.0;
+		for (int n_index = 0; n_index < (int)wrapper->world_model->final_networks.size(); n_index++) {
+			vector<double> inputs;
+			for (int i_index = 0; i_index < (int)wrapper->world_model->final_network_inputs[n_index].size(); i_index++) {
+				inputs.push_back(state[wrapper->world_model->final_network_inputs[n_index][i_index]]);
+			}
+			wrapper->world_model->final_networks[n_index]->activate(inputs);
+			sum_score += wrapper->world_model->final_networks[n_index]->output->acti_vals[0];
+		}
+
+		cout << "R6 predicted: " << sum_score << endl;
+	}
 }
