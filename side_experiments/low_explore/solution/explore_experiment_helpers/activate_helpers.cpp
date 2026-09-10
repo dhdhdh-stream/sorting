4e82d42f6bb0004c5d4763df5fc6410276e524de
dhdhdh <dhdhdhstream@gmail.com>
2026-09-10 01:34:24 -0400

    Day #1467: Low explore. Trying to limit number of blind explores/new tries.

---
+++ side_experiments/low_explore/solution/explore_experiment_helpers/activate_helpers.cpp
@@ -0,0 +1,101 @@
+#include "explore_experiment.h"
+
+#include "constants.h"
+#include "globals.h"
+#include "problem.h"
+#include "scope.h"
+#include "solution_helpers.h"
+#include "solution_wrapper.h"
+
+using namespace std;
+
+void ExploreExperiment::experiment_check_activate(vector<double>& obs,
+												  SolutionWrapper* wrapper) {
+	map<ExploreExperiment*, ExploreExperimentHistory*>::iterator it =
+		wrapper->explore_experiment_histories.find(this);
+	if (it == wrapper->explore_experiment_histories.end()) {
+		it = wrapper->explore_experiment_histories.insert({this, new ExploreExperimentHistory(this)}).first;
+	}
+	it->second->num_instances++;
+
+	switch (this->state) {
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_EXISTING:
+		train_existing_check_activate(obs,
+									  it->second,
+									  wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_EXPLORE:
+		explore_check_activate(obs,
+							   it->second,
+							   wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_NEW:
+		train_new_check_activate(obs,
+								 it->second,
+								 wrapper);
+		break;
+	}
+}
+
+void ExploreExperiment::experiment_step(vector<double>& obs,
+										int& action,
+										bool& is_next,
+										bool& fetch_action,
+										SolutionWrapper* wrapper) {
+	switch (this->state) {
+	case EXPLORE_EXPERIMENT_STATE_EXPLORE:
+		explore_step(obs,
+					 action,
+					 is_next,
+					 fetch_action,
+					 wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_NEW:
+		train_new_step(obs,
+					   action,
+					   is_next,
+					   wrapper);
+		break;
+	}
+}
+
+void ExploreExperiment::set_action(int action,
+								   SolutionWrapper* wrapper) {
+	explore_set_action(action,
+					   wrapper);
+}
+
+void ExploreExperiment::experiment_exit_step(SolutionWrapper* wrapper) {
+	switch (this->state) {
+	case EXPLORE_EXPERIMENT_STATE_EXPLORE:
+		explore_exit_step(wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_NEW:
+		train_new_exit_step(wrapper);
+		break;
+	}
+}
+
+void ExploreExperiment::backprop(double target_val,
+								 ExploreExperimentHistory* history,
+								 SolutionWrapper* wrapper) {
+	this->average_instances_per_hit = 0.99*this->average_instances_per_hit + 0.01*history->num_instances;
+
+	switch (this->state) {
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_EXISTING:
+		train_existing_backprop(target_val,
+								history,
+								wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_EXPLORE:
+		explore_backprop(target_val,
+						 history,
+						 wrapper);
+		break;
+	case EXPLORE_EXPERIMENT_STATE_TRAIN_NEW:
+		train_new_backprop(target_val,
+						   history,
+						   wrapper);
+		break;
+	}
+}
---
+++ side_experiments/low_explore/utils/constants.h
@@ -0,0 +1,44 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double MIN_WEIGHT = 0.00001;
+const double MIN_STANDARD_DEVIATION = 0.00001;
+
+/**
+ * - when there's correlation, weights can get strange values(?)
+ */
+const double REGRESSION_WEIGHT_LIMIT = 100000.0;
+
+/**
+ * - simply give raw actions a fixed weight
+ *   - cannot track success/count if continuous
+ *   - raw actions can also drive innovation anyways
+ */
+const int RAW_ACTION_WEIGHT = 8;
+
+const int STEP_TYPE_ACTION = 0;
+const int STEP_TYPE_SCOPE = 1;
+
+#if defined(MDEBUG) && MDEBUG
+const int NUM_LAST_TRACK = 4;
+const int MIN_NUM_LAST_TRACK = 2;
+const double LAST_BETTER_THAN_RATIO = 0.5;
+#else
+const int NUM_LAST_TRACK = 10;
+const int MIN_NUM_LAST_TRACK = 3;
+const double LAST_BETTER_THAN_RATIO = 0.6;
+#endif /* MDEBUG */
+
+#if defined(MDEBUG) && MDEBUG
+const int RAMP_NUM_ITERS = 100;
+#else
+const int RAMP_NUM_ITERS = 10000;
+#endif /* MDEBUG */
+
+const int GENERALIZE_ITER = 3;
+
+const int EXPERIMENT_REFRESH_NUM_ITERS = 10;
+
+const int STUCK_NUM_ITERS = 12;
+
+#endif /* CONSTANTS_H */
